8513ce2791ec7834c84b46fe31d3ce21213b25e3
danielgrigsby03 <daniel.grigsby.03@gmail.com>
2022-09-21 18:12:50 -0400

    Updated Main file to use javafx

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
-//main file
+package snake;
+
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("WOrking");
-        System.out.println("chad barns");
-        System.out.println("YOWYOJRWOJI");
+        try {
+            Application.launch(snake.class, args);
+        } catch (UnsupportedOperationException e) {
+            System.out.print(e);
+            System.err.println("Program is closing");
+            System.exit(1);
+        }
     }
 }
