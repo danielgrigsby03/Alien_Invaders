a92d3189ec885f8e0bc31238ef2bafbbb135b2a2
KevintrDo <kevintrando03@gmail.com>
2022-09-21 18:18:37 -0400

    Skeelton construtor, ini, start

--- snake.java
+++ snake.java
@@ -4,5 +4,21 @@ import javafx.application.Application;
 
 public class snake extends Application 
 {
+private Stage stage;
+private Scence scene; 
 
+    public snake() 
+    {
+
+    } //  snake constructor
+
+    public init() 
+    {
+
+    }// initialize 
+
+    public void start(Stage start) 
+    {
+
+    }// start
 } // snake class
