4c6d492c8d4094e33a12c8dd3193f20500ed2bee
KevintrDo <kevintrando03@gmail.com>
2022-09-28 17:43:23 -0400

    Web

--- snake.java
+++ snake.java
@@ -1,6 +1,10 @@
 package snake;
 
 import javafx.application.Application;
+import javafx.scene.scene;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
 
 public class snake extends Application 
 {
@@ -9,7 +13,8 @@ private Scence scene;
 
     public snake() 
     {
-
+        this.stage = null;
+        this.scene = null;
     } //  snake constructor
 
     public init() 
@@ -19,6 +24,16 @@ private Scence scene;
 
     public void start(Stage start) 
     {
+        this.stage = stage;
+        this.stage.setMaxWidth(750);
+        this.stage.setMaxHeight(750);
+        this.scene = new Scene(this.root,575,600);
+        this.stage.setOnCloseRequest(event -> Platform.exit());
+        this.stage.setTitle("GalleryApp!");
+        this.stage.setScene(this.scene);
+        this.stage.sizeToScene();
+        this.stage.show();
+        Platform.runLater(() -> this.stage.setResizable(false));
 
     }// start
 } // snake class
